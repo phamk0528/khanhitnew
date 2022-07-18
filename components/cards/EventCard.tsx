783fcb3bfcd71072d4e04622cdcb5301ed29132f
Khanh Pham <phamk0528@gmail.com>
2022-07-18 15:58:15 +0700

    Updatess

--- components/cards/EventCard.tsx
+++ components/cards/EventCard.tsx
@@ -67,7 +67,7 @@ const EventCard = ({
                     <Image
                         width={screenSize?.width > 500 ? "100%" : "180px"}
                         height={screenSize?.width > 500 ? "100%" : "150px"}
-                        src={getUrlImage(event?.photos[1]?.url.replace('https://playitright.s3-ap-southeast-1.amazonaws.com/', 'https://quocbcx-1c878.kxcdn.com/') + screenSize?.width > 500 ? '?width=550&quality=100' : `?width=300&quality=80` ?? '/placeholder.png')}
+                        src={getUrlImage(event?.photos[1]?.url.replace('https://playitright.s3-ap-southeast-1.amazonaws.com/', 'https://quocbcx-1c878.kxcdn.com/') + (event?.photos[1]?.url ? screenSize?.width > 500 ? '?width=550&quality=100' : `?width=300&quality=80` : null) ?? '/placeholder.png')}
                         alt={'Photo of ' + event?.title}
                         // objectFit="cover"
                         layout='fill'
