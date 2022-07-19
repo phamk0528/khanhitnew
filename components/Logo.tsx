997fd598e331c35c117dcbb1d2b2c9d37782cdc4
Khanh Pham <phamk0528@gmail.com>
2022-07-19 11:19:18 +0700

    Update

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,12 +4,13 @@ import Image from 'next/image'
 
 const Logo = (props: any) => {
   return (
-    <Box {...props}>
+    <Box {...props} width={"150px"} height="150px" position={'relative'}>
       <Image
         src={'https://quocbcx-1c878.kxcdn.com/assets/1/content/Logo-image-desktop-HUP.png?width=250&quality=80'}
         objectFit="contain"
         width={"150px"}
         height="150px"
+        layout="fill"
         alt='logo'
       />
     </Box>
