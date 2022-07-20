617a0f5a7ad76403f20b30348ece560f4a468c8d
Khanh Pham <phamk0528@gmail.com>
2022-07-20 11:36:36 +0700

    Update

--- fonts/index.tsx
+++ fonts/index.tsx
@@ -8,7 +8,7 @@ export const Fonts = () => (
     font-family: 'slick';
     font-style: normal;
             font-weight: 400;
-            src: local('Pacifico Regular'), local('Pacifico-Regular'), url(https://fonts.gstatic.com/s/pacifico/v12/FwZY7-Qmy14u9lezJ-6H6MmBp0u-.woff2) format('woff2');
+            src: local('Pacifico Regular'), local('/fonts/arial.ttf') format('ttf');
             font-display: swap;
         }
     `}
