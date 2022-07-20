88572df387dd82b4528a23cfd86806b7aa433aea
Khanh Pham <phamk0528@gmail.com>
2022-07-20 12:00:40 +0700

    Update

--- fonts/index.tsx
+++ fonts/index.tsx
@@ -1,16 +1,5 @@
 import { Global } from '@emotion/react';
 
 export const Fonts = () => (
-    <Global
-        styles={`
-
-    @font-face{
-    font-family: 'slick';
-    font-style: normal;
-            font-weight: 400;
-            src: local('Pacifico Regular'), local('/fonts/roboto-regular.woff2') format('woff2');
-            font-display: swap;
-        }
-    `}
-    />
+    <></>
 );
